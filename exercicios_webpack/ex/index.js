db6f32956b17f34e3cd7085598744dbba103d105
VitorSilvaSouza <vitorcontato.ti@gmail.com>
2018-06-26 16:03:58 -0300

    Terminado de transpilar o babel para o navegador. Subindo pasta do projeto para o GH.

---
+++ exercicios_webpack/ex/index.js
@@ -0,0 +1,4 @@
+import Pessoa from './pessoa'
+
+const pessoa = new Pessoa('Guilherme2')
+console.log(pessoa.toString());
---
+++ exercicios_webpack/webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './ex/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8081,
+        contentBase: './public'
+    },
+    module: {
+        loaders: [{
+            test: /.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['env']
+             // presets: ['es2015']
+            }
+        }]
+    }
+}
