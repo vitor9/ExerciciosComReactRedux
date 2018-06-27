08ee78494c05084272d6728d651e042885b8a8c7
VitorSilvaSouza <vitorcontato.ti@gmail.com>
2018-06-27 14:15:02 -0300

    S3 A23. Tornando nosso componente mais simples com arrow function.

--- exercicios_react/ex/component.jsx
+++ exercicios_react/ex/component.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
-export default function() {
-    return <h1>Primeiro Componente!</h1>
-}
+
+// export default () => <h1>Primeiro Componente!</h1>
+// Parenteses e chaves são diferentes, chave é o corpo
+// do método, e os parenteses são uma expressão a ser retornada
+
+export default () => ( 
+    <h1>Primeiro Componente!</h1> 
+)
