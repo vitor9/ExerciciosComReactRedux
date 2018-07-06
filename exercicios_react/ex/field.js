02c4e6886a5b39e34608b729d80c2b631c6dc65e
Vitor Silva Souza <vtorzy@gmail.com>
2018-07-06 11:35:05 -0300

    S6 A66 - Integrando o React-Redux

--- exercicios_react/ex/field.js
+++ exercicios_react/ex/field.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Field extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { value: props.initialValue }
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value })
-  }
 
   render() {
     return (
       <div>
-        <label> {this.state.value} </label><br />
-        <input onChange={this.handleChange} value={this.state.value}/>
+        <label> {this.props.value} </label><br />
+        <input onChange={this.handleChange} value={this.props.value}/>
       </div>
     )
   }
 }
 
-export default Field
+function mapStateToProps(state) {
+  return {
+    value: state.field.value
+  }
+}
+
+// Padrao de Projeto chamado Decorator
+// Passamos um field e retornamos o mesmo field, só que decorado com as mesmas funcionalidades
+// mapeado para os estados globais da aplicacao
+export default connect(mapStateToProps)(Field)
