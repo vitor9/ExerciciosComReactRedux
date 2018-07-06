769cffea9db79ea5f8dfe2225fceb5623409a878
Vitor Silva Souza <vtorzy@gmail.com>
2018-07-06 12:15:43 -0300

    S6 A67/8 pt1

--- exercicios_react/ex/field.js
+++ exercicios_react/ex/field.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeValue } from './fieldActions.js';
 
 class Field extends Component {
-
-  render() {
-    return (
-      <div>
-        <label> {this.props.value} </label><br />
-        <input onChange={this.handleChange} value={this.props.value}/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <label> {this.props.value} </label><br />
+                <input onChange={this.props.changeValue} value={this.props.value} />
+            </div>
+        )
+    }
 }
 
 function mapStateToProps(state) {
-  return {
-    value: state.field.value
-  }
+    return {
+        value: state.field.value
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ changeValue }, dispatch)
 }
 
 // Padrao de Projeto chamado Decorator
 // Passamos um field e retornamos o mesmo field, só que decorado com as mesmas funcionalidades
 // mapeado para os estados globais da aplicacao
-export default connect(mapStateToProps)(Field)
+export default connect(mapStateToProps, mapDispatchToProps)(Field)
