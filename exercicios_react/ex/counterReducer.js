4aa275c9a77e78d9ffe6644a91da48fcf4bb3d1d
Vitor Silva Souza <vtorzy@gmail.com>
2018-07-10 14:35:33 -0300

    S6 A70 Terminado o exercicio.

--- exercicios_react/ex/counterReducer.js
+++ exercicios_react/ex/counterReducer.js
@@ -1,16 +1,13 @@
 const INITIAL_STATE = { step: 1, number: 0 };
 
-// Se state vier como undefined, ele vai assumir o INITIAL_STATE
-// Se for outro valor, elevai pegar o valor que foi passado.
 export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
         case 'INC':
-        //Pegar o estado atual e mudar o number
-            return{...state, number: state.number + state.step}
+            return {...state, number: state.number + state.step}
         case 'DEC':
-            return { ...state, number: state.number + state.step }
+            return { ...state, number: state.number - state.step }
         case 'STEP_CHANGED':
-            return { ...state, step: action.payload }
+            return { ...state, step: +action.payload }
         default:
             return state
     }
